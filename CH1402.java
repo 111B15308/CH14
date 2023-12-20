f47adf0ee4f3dd669850c9646f00d9f06121455b
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-20 21:16:44 +0800

    Update CH1402.java

--- CH1402.java
+++ CH1402.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UpperCaseConverter {
+public class CH1402{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
