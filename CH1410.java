90f9b6ee15e8dc117538d37e15de4d83c44cba14
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-24 21:19:49 +0800

    Update CH1410.java

--- CH1410.java
+++ CH1410.java
@@ -5,14 +5,12 @@ public class CH1410{
     private int english;
     private int math;
 
-    // (a) 有引數的建構子
     public Data(String str, int e, int m) {
         this.name = str;
         this.english = e;
         this.math = m;
     }
 
-    // (b) 將物件資料寫入 student.txt
     public void writeData() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("student.txt", true))) {
             writer.write(name + " " + english + " " + math);
@@ -22,7 +20,6 @@ public class CH1410{
         }
     }
 
-    // (c) 顯示資料及平均分數
     public void show() {
         System.out.println("Name: " + name);
         System.out.println("English Score: " + english);
@@ -36,7 +33,6 @@ public class CH1410{
         return (english + math) / 2.0;
     }
 
-    // (d) 讀取 student.txt 資料並顯示
     public static void readData() {
         try (BufferedReader reader = new BufferedReader(new FileReader("student.txt"))) {
             String line;
@@ -46,10 +42,8 @@ public class CH1410{
                 int english = Integer.parseInt(data[1]);
                 int math = Integer.parseInt(data[2]);
 
-                // 建立 Data 物件
                 Data student = new Data(name, english, math);
 
-                // 顯示資料
                 student.show();
             }
         } catch (IOException | NumberFormatException e) {
@@ -58,7 +52,7 @@ public class CH1410{
     }
 
     public static void main(String[] args) {
-        // (e) 建立學生物件並寫入 student.txt
+
         Data stu1 = new Data("Ariel", 92, 85);
         stu1.writeData();
 
